4ddeb3da9493a1b4cf2698c0d9d3befec6737af5
YiranZhou-Robotics <yiran_zhou23@163.com>
2025-06-18 03:21:16 +0000

    Deploying to gh-pages from @ YiranZhou-Robotics/YiranZhou-Robotics.github.io@66b5d2b6cb178f646a25c98880a5505e12f09bb1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,35 +461,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%79%69%72%61%6E.%7A%68%6F%75-%31@%73%74%75%64%65%6E%74.%75%74%73.%65%64%75.%61%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://github.com/YiranZhou-Robotics", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=MmLEw6EAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
